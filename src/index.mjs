2568708f2961c7547d7db1aa44db51824911d407
amtenu <amannov21@gmail.com>
2024-02-17 11:36:35 -0700

    Separated data and user params to search by params

--- src/index.mjs
+++ src/index.mjs
@@ -4,18 +4,38 @@ const app = express();
 
 const PORT = process.env.PORT | 3000;
 
+const allUsers = [
+  {
+    id: 1,
+    name: "Amanuel",
+  },
+  { id: 2, name: "Solomon" },
+];
+
 app.get("/", (req, res) => {
   res.status(201).send("Hello world");
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "Amanuel",
-    },
-    { id: 1, name: "Solomon" },
-  ]);
+  res.send(allUsers);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return res.status(400).send({ msg: "Bad Request.InvalidId" });
+  }
+  
+  const findUser = allUsers.find((user) => {
+  return  user.id === parsedId;
+  });
+
+  console.log(findUser);
+
+  if (!findUser) return res.sendStatus(404);
+
+  return res.send(findUser);
 });
 
 app.get("/api/products", (req, res) => {
