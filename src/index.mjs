9e3b7f0d6ba2e7300797bab636cd75c9d78cfc18
amtenu <amannov21@gmail.com>
2024-02-17 11:13:19 -0700

    root, users and products endpoints

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,33 @@
 import express from "express";
 
-const app=express();
+const app = express();
 
 const PORT = process.env.PORT | 3000;
 
+app.get("/", (req, res) => {
+  res.status(201).send("Hello world");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Running on Port ${PORT}`)
+app.get("/api/users", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "Amanuel",
+    },
+    { id: 1, name: "Solomon" },
+  ]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "Sugar",
+    },
+    { id: 2, name: "Tea" },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
 });
