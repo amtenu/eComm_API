a9b0f8eb47d8c9072340e553c4452df536d23001
amtenu <amannov21@gmail.com>
2024-02-23 11:29:13 -0700

    validation schema for age

--- src/utils/validationSchemas.mjs
+++ src/utils/validationSchemas.mjs
@@ -1,17 +1,32 @@
-export const createUserValidationSchema={
-    name:{
-        isLength:{
-            options:{
-                min:5,
-                max:32
-            },
-            errorMessage:"username must be between 2 and 32"
-        },
-        notEmpty:{
-            errorMessage:"Not be empty"
-        },
-        isString:{
-            errorMessage:"Only Strings"
-        }
-    }
-}
+export const createUserValidationSchema = {
+  name: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage: "username must be between 2 and 32",
+    },
+    notEmpty: {
+      errorMessage: "Not be empty",
+    },
+    isString: {
+      errorMessage: "Only Strings",
+    },
+  },
+  age: {
+    isLength: {
+      options: {
+        min: 1,
+        max: 3,
+      },
+      errorMessage: "Age must be between 1 and 100",
+    },
+    notEmpty: {
+      errorMessage: "Not be empty",
+    },
+    isNumeric: {
+      errorMessage: "Only Numbers",
+    },
+  },
+};
